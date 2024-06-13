c17affcc6829ffe6346a297cfc682de91baf313b
MihailBN <aratparosan@gmail.com>
2024-06-13 12:40:27 +0500

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,6 @@
+immutable_var = (1, 2, 'a', 'b')
+print(immutable_var)
+# immutable_var [1] = 5 кортеж не поддерживает замену элементов
+Mutable_list = [1, 2, 'a', 'b']
+Mutable_list.append('Modified')
+print(Mutable_list)
